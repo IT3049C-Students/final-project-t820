7c4696ee8d65301e9adab5c337cf53ddd8e5bfbc
schriewer11 <schriewer11@yahoo.com>
2022-08-02 13:59:39 -0400

    added window.onload function to game.js

--- scripts/game.js
+++ scripts/game.js
@@ -1,15 +1,19 @@
-let config = {
-    width: 800,
-    height: 600,
-    backgroundColor: 0x15A1EB,
-    autoCenter: true,
-    scene: [ Scene1, Scene2 ],
-    physics: {
-        default: 'arcade',
-        arcade: {
-            debug: false,
+window.onload = function() {
+
+    let config = {
+        width: 800,
+        height: 600,
+        backgroundColor: 0x15A1EB,
+        autoCenter: true,
+        scene: [ Scene1, Scene2 ],
+        physics: {
+            default: 'arcade',
+            arcade: {
+                debug: false,
+            }
         }
     }
+
+    let game = new Phaser.Game( config );
 }
 
-let game = new Phaser.Game( config );
