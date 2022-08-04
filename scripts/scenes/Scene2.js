9fd9f21928ce0ae88c07e5d8007bc5b225d4339c
Tara <tarapoudyel999@gmail.com>
2022-08-04 17:30:40 -0400

    Update Scene2.js

--- scripts/scenes/Scene2.js
+++ scripts/scenes/Scene2.js
@@ -7,16 +7,20 @@ class Scene2 extends Phaser.Scene {
     create() {
 
         this.apple = this.physics.add.sprite(config.width / 2 - 50, config.height / 2, "apple");
-
+        this.apple.setVelocityX(0);
+        this.apple.setVelocityY(0);
+        
         let appleCount = 0;
         globalThis.score = 0;
         globalThis.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
-        this.snake = this.physics.add.sprite( config.width / 2 , config.height / 2, 'snake' );
+        //this.snake = this.physics.add.sprite( config.width / 2 , config.height / 2, 'snake' );
+        this.snake = this.add.rectangle(config.width / 2 - 50, config.height / 2, 25, 25, 0x00ff00);
+        this.physics.add.existing(this.snake);
 
         this.cursorKeys = this.input.keyboard.createCursorKeys();
 
-        this.snake.setCollideWorldBounds();
+        this.snake.body.setCollideWorldBounds();
 
         this.physics.add.collider(this.snake, this.apple, this.collectApple, null, this)
     
@@ -32,37 +36,39 @@ class Scene2 extends Phaser.Scene {
     snakeMoveManager() {
         // left and right
         if ( this.cursorKeys.left.isDown ) {
-            this.snake.setVelocityY( 0 );
-            this.snake.setVelocityX( -gameSettings.playerSpeed );
+            this.snake.body.setVelocityY( 0 );
+            this.snake.body.setVelocityX( -gameSettings.playerSpeed );
         } else if ( this.cursorKeys.right.isDown ) {
-            this.snake.setVelocityY( 0 );
-            this.snake.setVelocityX( gameSettings.playerSpeed );
+            this.snake.body.setVelocityY( 0 );
+            this.snake.body.setVelocityX( gameSettings.playerSpeed );
         }
 
         // up and down
         if ( this.cursorKeys.up.isDown ) {
-            this.snake.setVelocityX( 0 );
-            this.snake.setVelocityY( -gameSettings.playerSpeed );
+            this.snake.body.setVelocityX( 0 );
+            this.snake.body.setVelocityY( -gameSettings.playerSpeed );
         } else if ( this.cursorKeys.down.isDown ) {
-            this.snake.setVelocityX( 0 );
-            this.snake.setVelocityY( gameSettings.playerSpeed );
+            this.snake.body.setVelocityX( 0 );
+            this.snake.body.setVelocityY( gameSettings.playerSpeed );
         }
 
     }
 
-    collectApple(snake, apple2) {
+    collectApple(snake, apple) {
         score += 10;
         scoreText.setText('Score: ' + score);
-        this.resetApplePos(apple2);
+        this.resetApplePos(apple);
     }
 
-    resetApplePos(apple3) {
+    resetApplePos(apple) {
         
         let y = Math.floor(Math.random() * config.height + 40) - 40; 
         let x = Math.floor(Math.random() * config.width + 40) - 40; 
         console.log(x + "    " + y);
-        apple3.y = y;
-        apple3.x = x;
+        apple.setVelocityX(0);
+        apple.setVelocityY(0);
+        apple.y = y;
+        apple.x = x;
     }
 
 
