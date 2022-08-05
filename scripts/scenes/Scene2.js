f863a96e011fedf0c19f809574bd4272cbb3bd06
Tara <tarapoudyel999@gmail.com>
2022-08-04 21:38:33 -0400

    Added proper snake movement

--- scripts/scenes/Scene2.js
+++ scripts/scenes/Scene2.js
@@ -2,6 +2,9 @@ class Scene2 extends Phaser.Scene {
 
     constructor() {
         super( 'playGame' );
+        this.lastMoveTime = 0;
+        this.timeInterval = 300;
+        this.direction = "right"
     }
 
     create() {
@@ -14,44 +17,51 @@ class Scene2 extends Phaser.Scene {
         globalThis.score = 0;
         globalThis.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
-        //this.snake = this.physics.add.sprite( config.width / 2 , config.height / 2, 'snake' );
-        this.snake = this.add.rectangle(config.width / 2 - 50, config.height / 2, 25, 25, 0x00ff00);
+        this.snake = this.physics.add.sprite( config.width / 2 , config.height / 2, 'snake' );
+
+        // Tried using a green rectangle as a snake thinking it might make it easier to add a growth function.
+        
+        /*this.snake = this.add.rectangle(config.width / 2 - 50, config.height / 2, 25, 25, 0x00ff00);
         this.physics.add.existing(this.snake);
+        */
 
         this.cursorKeys = this.input.keyboard.createCursorKeys();
 
-        this.snake.body.setCollideWorldBounds();
+        this.snake.setCollideWorldBounds();
 
-        this.physics.add.collider(this.snake, this.apple, this.collectApple, null, this)
-    
+        this.physics.add.collider(this.snake, this.apple, this.collectApple, null, this);
+
     }
 
-    update() {
-        this.snakeMoveManager();
-        if(this.snake) {
-
-        }
-    }
-
-    snakeMoveManager() {
-        // left and right
+    update(time) {
         if ( this.cursorKeys.left.isDown ) {
-            this.snake.body.setVelocityY( 0 );
-            this.snake.body.setVelocityX( -gameSettings.playerSpeed );
+            this.direction = "left"
         } else if ( this.cursorKeys.right.isDown ) {
-            this.snake.body.setVelocityY( 0 );
-            this.snake.body.setVelocityX( gameSettings.playerSpeed );
+            this.direction = "right"
         }
 
         // up and down
         if ( this.cursorKeys.up.isDown ) {
-            this.snake.body.setVelocityX( 0 );
-            this.snake.body.setVelocityY( -gameSettings.playerSpeed );
+            this.direction = "up"
         } else if ( this.cursorKeys.down.isDown ) {
-            this.snake.body.setVelocityX( 0 );
-            this.snake.body.setVelocityY( gameSettings.playerSpeed );
+            this.direction = "down"
         }
+        if(time > this.lastMoveTime + this.timeInterval) {
+            this.lastMoveTime = time;
+            this.snakeMoveManager(this.direction);
+        }
+    }
 
+    snakeMoveManager(direction) {
+            if(this.direction == "left") {
+                this.snake.x -= gameSettings.gridWidth;
+                } else if (direction =="right") {
+                this.snake.x += gameSettings.gridWidth;
+                } else if (direction == "down") {
+                this.snake.y += gameSettings.gridWidth;
+                } else {
+                this.snake.y -= gameSettings.gridWidth;
+        }
     }
 
     collectApple(snake, apple) {
